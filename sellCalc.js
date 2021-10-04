ab4f7eda53f17f6ac8793275c81f0074303d263b
minersinn <rglm14@gmail.com>
2021-10-04 17:27:54 -0400

    Consolidate and function update
    Consolidated both calculators to 1 JS file and now display entire results split by line break.

--- sellCalc.js
+++ sellCalc.js
@@ -1,4 +1,3 @@
-//option 2 - allow the user to type in the quantity of each item they have and the calculator would return a simple total in some detailed format.
 //Object constructor for the items that User will sell to Corp
 function Item(name, priceUnit, bonus) {
     this.itemName = name;
@@ -7,7 +6,7 @@ function Item(name, priceUnit, bonus) {
     this.output = function() {
         return this.itemName + ": (" + this.itemPrice + " ISK)";
     };
-}
+};
 //Items available (objects) ***UPDATE PRICES HERE***
     const veld = new Item("Veldspar", 10.30, "0%");
     const conVeld = new Item("Concentrated Veldspar", 11.07, "5%");
@@ -25,8 +24,9 @@ function Item(name, priceUnit, bonus) {
 function itemDisplay() {
     document.getElementById("dispItems").innerHTML = 
 '<form><label for="veld1">' + veld.output() + '</label><br><input type="number" name="veld1" id="veld1" value="0" autofocus><br><label for="veld2">' + conVeld.output() + '</label><br><input type="number" name="veld2" id="veld2" value="0"><br><label for="veld3">' + denseVeld.output() + '</label><br><input type="number" name="veld3" id="veld3" value="0"><br><br><label for="scor1">' + scor.output() + '</label><br><input type="number" name="scor1" id="scor1" value="0"><br><label for="scor2">' + conScor.output() + '</label><br><input type="number" name="scor2" id="scor2" value="0"><br><label for="scor3">' + massScor.output() + '</label><br><input type="number" name="scor3" id="scor3" value="0"><br><br><label for="pyro1">' + prox.output() + '</label><br><input type="number" name="pyro1" id="pyro1" value="0"><br><label for="pyro2">' + solProx.output() + '</label><br><input type="number" name="pyro2" id="pyro2" value="0"><br><label for="pyro3">' + visProx.output() + '</label><br><input type="number" name="pyro3" id="pyro3" value="0"><br><br><button type="button" onclick="option2()">Calculate</button><button type="reset">Reset</button></form>'
-}
+};
 
+//option 2 - allow the user to type in the quantity of each item they have and the calculator would return a simple total in some detailed format.
 //Calculator
 function option2() {
     let veldQty = parseInt(document.getElementById("veld1").value);
@@ -57,7 +57,85 @@ function option2() {
     
     document.getElementById("result").innerHTML = "<br><strong>Veldspar</strong><br>" + veld.itemName + ": (" + veldQty + ") units = " + veldTot.toFixed(2) + " ISK<br>" + conVeld.itemName + ": (" + conveldQty + ") units = " + conveldTot.toFixed(2) + " ISK<br>" + denseVeld.itemName + ": (" + denveldQty + ") units = " + denveldTot.toFixed(2) + " ISK<br><br><strong>Scordite</strong><br>" + scor.itemName + ": (" + scorQty + ") units = " + scorTot.toFixed(2) + " ISK<br>" + conScor.itemName + ": (" + conscorQty + ") units = " + conscorTot.toFixed(2) + " ISK<br>" + massScor.itemName + ": (" + mascorQty + ") units = " + mascorTot.toFixed(2) + " ISK<br><br><strong>Pyroxeres</strong><br>"  + prox.itemName + ": (" + proxQty + ") units = " + pyroTot.toFixed(2) + " ISK<br>" + solProx.itemName + ": (" + solproxQty + ") units = " + solpyroTot.toFixed(2) + " ISK<br>" + visProx.itemName + ": (" + visproxQty + ") units = " + vispyroTot.toFixed(2) + " ISK<br><strong>Contract Total: " + grandTot.toFixed(2) + " ISK<br>";
     //alert(grandTot.toFixed(2) + " ISK")
-}
+};
+
+// THIS IS THE START OF OPT 1 (THE LOOT CALC)
+function byElements() {
+    let itemTot;
+    let useroutput = [];
+    let lnAnswer;
+    let userInput = document.getElementById("inp").value;
+    let data = userInput.trim();
+    let rows = data.split("\n");
+
+    for (let i = 0; i < rows.length; i++) {
+        const row = rows[i];
+        //console.log(row);
+        let elements = row.split("\t");
+        //console.log(elements[2]);
+        switch (elements[2]) {
+            case veld.itemName:
+                itemTot = parseInt(elements[3]) * veld.itemPrice;
+                console.log(elements[1] + " " + elements[2] + " (" + elements[3] + ")units " + itemTot.toFixed(2) + " ISK");
+                lnAnswer = elements[1] + " " + elements[2] + " (" + elements[3] + ")units " + itemTot.toFixed(2) + " ISK<br>";
+                break;
+        
+            case conVeld.itemName:
+                itemTot = parseInt(elements[3]) * conVeld.itemPrice;
+                console.log(elements[1] + " " + elements[2] + " (" + elements[3] + ")units " + itemTot.toFixed(2) + " ISK");            
+                lnAnswer = elements[1] + " " + elements[2] + " (" + elements[3] + ")units " + itemTot.toFixed(2) + " ISK<br>";
+                break;
+    
+            case denseVeld.itemName:
+                itemTot = parseInt(elements[3]) * denseVeld.itemPrice;
+                console.log(elements[1] + " " + elements[2] + " (" + elements[3] + ")units " + itemTot.toFixed(2) + " ISK");        
+                lnAnswer = elements[1] + " " + elements[2] + " (" + elements[3] + ")units " + itemTot.toFixed(2) + " ISK<br>";
+                break;
+        
+            case scor.itemName:
+                itemTot = parseInt(elements[3]) * scor.itemPrice;
+                console.log(elements[1] + " " + elements[2] + " (" + elements[3] + ")units " + itemTot.toFixed(2) + " ISK");        
+                lnAnswer = elements[1] + " " + elements[2] + " (" + elements[3] + ")units " + itemTot.toFixed(2) + " ISK<br>";
+                break;
+    
+            case conScor.itemName:
+                itemTot = parseInt(elements[3]) * conScor.itemPrice;
+                console.log(elements[1] + " " + elements[2] + " (" + elements[3] + ")units " + itemTot.toFixed(2) + " ISK");        
+                lnAnswer = elements[1] + " " + elements[2] + " (" + elements[3] + ")units " + itemTot.toFixed(2) + " ISK<br>";
+                break;
+    
+            case massScor.itemName:
+                itemTot = parseInt(elements[3]) * massScor.itemPrice;
+                console.log(elements[1] + " " + elements[2] + " (" + elements[3] + ")units " + itemTot.toFixed(2) + " ISK");            
+                lnAnswer = elements[1] + " " + elements[2] + " (" + elements[3] + ")units " + itemTot.toFixed(2) + " ISK<br>";
+                break;
+        
+            case prox.itemName:
+                itemTot = parseInt(elements[3]) * prox.itemPrice;
+                console.log(elements[1] + " " + elements[2] + " (" + elements[3] + ")units " + itemTot.toFixed(2) + " ISK");            
+                lnAnswer = elements[1] + " " + elements[2] + " (" + elements[3] + ")units " + itemTot.toFixed(2) + " ISK<br>";
+                break;
+    
+            case solProx.itemName:
+                itemTot = parseInt(elements[3]) * solProx.itemPrice;
+                console.log(elements[1] + " " + elements[2] + " (" + elements[3] + ")units " + itemTot.toFixed(2) + " ISK");        
+                lnAnswer = elements[1] + " " + elements[2] + " (" + elements[3] + ")units " + itemTot.toFixed(2) + " ISK<br>";
+                break;
+        
+            case visProx.itemName:
+                itemTot = parseInt(elements[3]) * visProx.itemPrice;
+                console.log(elements[1] + " " + elements[2] + " (" + elements[3] + ")units " + itemTot.toFixed(2) + " ISK");        
+                lnAnswer = elements[1] + " " + elements[2] + " (" + elements[3] + ")units " + itemTot.toFixed(2) + " ISK<br>";
+                break;
+    
+            default:
+                break;
+        };
+        useroutput.push(lnAnswer);
+    };        
+    document.getElementById("output").innerHTML = useroutput;
+
+};
 
 /*
 Regular space: &nbsp;
