4c525be0d8e2378b622775d56c9b51277c36f830
minersinn <rglm14@gmail.com>
2022-01-13 14:59:34 -0500

    Buy Calculator working again

--- buyCalc.js
+++ buyCalc.js
@@ -1,4 +1,42 @@
-            const TRITISK = 3.82;
+const changeDate = "1/9/2022";
+const priceList = {
+  //DO NOT USE COMMAS IN THE NUMBERS!!!!!!!!!!!
+  "Tritanium" : 3.82,
+  "Pyerite" : 14.85,
+  "Mexallon" : 47.32 
+};
+
+var currency = {
+  style: "currency",
+  currency: "USD"
+};
+
+let headerRow = ["Item", "Price"];
+let userArray = [];
+for (const key in priceList) {
+    let userline = "<tr><td>" +  key + "</td><td>" + priceList[key].toLocaleString("en-US", currency) + " ISK</td>";
+    userArray.push(userline);
+};
+let text = userArray.join("</tr>");
+
+
+document.querySelector(".list").innerHTML = "<h3 style='text-align: center'>Prices updated on: " + changeDate + "</h3><br><table id='priceTable'><tr><th>" + headerRow[0] + "</th><th> " + headerRow[1] + "</th></tr>" + text + "</table>";
+
+function priceCalc() {
+  let tritQTY = document.querySelector("#trit").value;
+  let tritTOT = tritQTY * priceList.Tritanium;
+  let pyQTY = document.querySelector("#py").value;
+  let pyTOT = pyQTY * priceList.Pyerite;
+  let mexQTY = document.querySelector("#mex").value;
+  let mexTOT = mexQTY * priceList.Mexallon;
+  let total = tritTOT + pyTOT + mexTOT
+  let ans = "Tritanium: (" + tritQTY + ") units " + tritTOT +" ISK <br>Pyerite: (" + pyQTY + ") units " + pyTOT + " ISK <br>Mexallon: (" + mexQTY + ") units " + mexTOT + " ISK <br><strong>Grand Total: " + total +" ISK</strong></div>";
+
+  document.querySelector("#output").innerHTML = ans;
+
+}
+
+/*            const TRITISK = 3.82;
             const PYISK = 14.85;
             const MEXISK = 47.32;
             var answer
@@ -15,13 +53,12 @@
               cpyanswer = " Trit:" + tritQty + " Pye:" + pyQty + " Mex:" + mexQty + " Contract Total:" + grandTot.toFixed(2);
 
             }
+*/
 
             function copyData() {
               var copyText = document.getElementById("cpy");
               copyText.select();
-              copyText.setSelectionRange(0, 99999); /* For mobile devices */
+              copyText.setSelectionRange(0, 99999);  /*For mobile devices */
               navigator.clipboard.writeText(copyText.value);
               alert("Copied the text: " + copyText.value);
-            }
-
-          
+            };
