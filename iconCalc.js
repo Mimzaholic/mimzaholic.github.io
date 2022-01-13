c531e0e02e6943fb8c134173a69165b5f3375e59
minersinn <rglm14@gmail.com>
2022-01-13 00:55:31 -0500

    Icon Calc

--- iconCalc.js
+++ iconCalc.js
@@ -1,35 +1,35 @@
 const changeDate = "1/9/2022";
 const priceList = {
     //DO NOT USE COMMAS IN THE NUMBERS!!!!!!!!
-    "Veldspar": 10.11,
-    "Concentrated Veldspar": 10.91,
-    "Dense Veldspar": 10.63,
+    "Veldspar": 12.29,
+    "Concentrated Veldspar": 11.14,
+    "Dense Veldspar": 11.07,
 
-    "Scordite": 17.97,
-    "Condensed Scordite": 18.87,
-    "Massive Scordite": 18.44,
+    "Scordite": 18.81,
+    "Condensed Scordite": 16.34,
+    "Massive Scordite": 18.9,
 
-    "Pyroxeres": 24.30,
-    "Solid Pyroxeres": 27.28,
-    "Viscous Pyroxeres": 26.82,
+    "Pyroxeres": 22.98,
+    "Solid Pyroxeres": 24.5,
+    "Viscous Pyroxeres": 23.49,
     //Moon Ores
-    "Bitumens": 1189.15,
-    "Brimful Bitumens": 1367.52,
-    "Glistening Bitumens": 2378.30,
+    "Bitumens": 1800,
+    "Brimful Bitumens": 2070,
+    "Glistening Bitumens": 2900,
 
-    "Coesite": 1264.80,
-    "Brimful Coesite": 1454.52,
-    "Glistening Coesite": 2529.60,
+    "Coesite": 1500,
+    "Brimful Coesite": 1725,
+    "Glistening Coesite": 2700,
 
-    "Sylvite": 1145.38,
-    "Brimful Sylvite": 1317.19,
-    "Glistening Sylvite": 2290.76,
+    "Sylvite": 1800,
+    "Brimful Sylvite": 2070,
+    "Glistening Sylvite": 3000,
 
-    "Zeolites": 1190.00,
-    "Brimful Zeolites": 1368.50,
-    "Glistening Zeolites": 2380.00,
+    "Zeolites": 1500,
+    "Brimful Zeolites": 1725,
+    "Glistening Zeolites": 2700,
     //Ice
-    "Clear Icicle": 186150.00
+    "Clear Icicle": 170820
 };
 
 var currency = {
@@ -46,18 +46,30 @@ for (const key in priceList) {
 let text = userArray.join("</tr>")
 
 
-document.querySelector(".list").innerHTML = "<h4 style='text-align: center'>Updated on: " + changeDate + "</h4><br><table id='priceTable'><tr><th>" + headerRow[0] + "</th><th> " + headerRow[1] + "</th></tr>" + text + "</table>";
+document.querySelector(".list").innerHTML = "<h3 style='text-align: center'>Prices updated on: " + changeDate + "</h3><br><table id='priceTable'><tr><th>" + headerRow[0] + "</th><th> " + headerRow[1] + "</th></tr>" + text + "</table>";
 
 function icon() {
-    let userInput = document.getElementById("inpBox").value;
+    let userInput = document.querySelector("#inpBox").value;
+    console.log(userInput);
     let data = userInput.trim();
     let rows = data.split("\n");
+    let reciept = [];
+    let answer = "";
+    let tot = 0;
 
     for (let i = 0; i < rows.length; i++) {
         const row = rows[i];
-        console.log(row);
         let elements = row.split("\t");
-        console.log(elements);
-        
-    }
+        let key = elements[0];
+        let lnprice = priceList[key] * elements[1];
+        let line = "<tr><td>" + elements[0] + "</td><td>" + lnprice.toLocaleString("en-US", currency) + " ISK</td>";
+        reciept.push(line);
+        console.log(lnprice);
+        tot += lnprice;
+        console.log(tot);
+
+    };
+    answer = "<table><th>Item</th><th>Total</th>" + reciept.join("</tr>") + "<tr><td><strong>Grand Total:</strong></td><td><strong>" + tot.toLocaleString("en-US",currency) + " ISK</strong></td></tr></table>";
+    document.querySelector("#sellOut").innerHTML = answer;
+
 };
